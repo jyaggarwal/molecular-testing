8c165a58dc120492b554234ae0379d1b2a96cdbb
jyaggarwal <112942342+jyaggarwal@users.noreply.github.com>
2022-11-17 15:18:44 +0530

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,18 +1,8 @@
 # edit molecule file
-rm -rf my_new_role/molecule/default/molecule.yml
-cat >> my_new_role/molecule/default/molecule.yml  "---"
-cat >> my_new_role/molecule/default/molecule.yml "dependency:"
-cat >> my_new_role/molecule/default/molecule.yml "  name: galaxy"
-cat >> my_new_role/molecule/default/molecule.yml "driver:"
-cat >> my_new_role/molecule/default/molecule.yml "  name: docker"
-cat >> my_new_role/molecule/default/molecule.yml "platforms:"
-cat >> my_new_role/molecule/default/molecule.yml "  - name: instance"
-cat >> my_new_role/molecule/default/molecule.yml "    image: quay.io/centos/centos:stream8"
-cat >> my_new_role/molecule/default/molecule.yml "    pre_build_image: true"
-cat >> my_new_role/molecule/default/molecule.yml "provisioner:"
-cat >> my_new_role/molecule/default/molecule.yml "  name: ansible"
-cat >> my_new_role/molecule/default/molecule.yml "  env:"
-cat >> my_new_role/molecule/default/molecule.yml "    ANSIBLE_ROLES_PATH: '../../roles'"
-cat >> my_new_role/molecule/default/molecule.yml "verifier:"
-cat >> my_new_role/molecule/default/molecule.yml "  name: ansible"
-EOF
+ed my_new_role/molecule/default/molecule.yml
+11i
+  env:
+    ANSIBLE_ROLES_PATH: "../../roles"
+
+
+
