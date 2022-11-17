c9f656196d0add9d51f098cb4079b9e8281bd922
jyaggarwal <112942342+jyaggarwal@users.noreply.github.com>
2022-11-17 13:15:53 +0530

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,19 +1,17 @@
 # edit molecule file
 rm -rf my_new_role/molecule/default/molecule.yml
-vim my_new_role/molecule/default/molecule.yml
-echo "---" >> my_new_role/molecule/default/molecule.yml
-echo "dependency:" >> my_new_role/molecule/default/molecule.yml
-echo "  name: galaxy" >> my_new_role/molecule/default/molecule.yml
-echo "driver:" >> my_new_role/molecule/default/molecule.yml
-echo "  name: docker" >> my_new_role/molecule/default/molecule.yml
-echo "platforms:" >> my_new_role/molecule/default/molecule.yml
-echo "  - name: instance" >> my_new_role/molecule/default/molecule.yml
-echo "    image: quay.io/centos/centos:stream8" >> my_new_role/molecule/default/molecule.yml
-echo "    pre_build_image: true" >> my_new_role/molecule/default/molecule.yml
-echo "provisioner:" >> my_new_role/molecule/default/molecule.yml
-echo "  name: ansible" >> my_new_role/molecule/default/molecule.yml
-echo "  env:" >> my_new_role/molecule/default/molecule.yml
-echo "    ANSIBLE_ROLES_PATH: '../../roles'" >> my_new_role/molecule/default/molecule.yml
-echo "verifier:" >> my_new_role/molecule/default/molecule.yml
-echo "  name: ansible" >> my_new_role/molecule/default/molecule.yml
-kill %1
+cat >> my_new_role/molecule/default/molecule.yml  "---"
+cat >> my_new_role/molecule/default/molecule.yml "dependency:"
+cat >> my_new_role/molecule/default/molecule.yml "  name: galaxy"
+cat >> my_new_role/molecule/default/molecule.yml "driver:"
+cat >> my_new_role/molecule/default/molecule.yml "  name: docker"
+cat >> my_new_role/molecule/default/molecule.yml "platforms:"
+cat >> my_new_role/molecule/default/molecule.yml "  - name: instance"
+cat >> my_new_role/molecule/default/molecule.yml "    image: quay.io/centos/centos:stream8"
+cat >> my_new_role/molecule/default/molecule.yml "    pre_build_image: true"
+cat >> my_new_role/molecule/default/molecule.yml "provisioner:"
+cat >> my_new_role/molecule/default/molecule.yml "  name: ansible"
+cat >> my_new_role/molecule/default/molecule.yml "  env:"
+cat >> my_new_role/molecule/default/molecule.yml "    ANSIBLE_ROLES_PATH: '../../roles'"
+cat >> my_new_role/molecule/default/molecule.yml "verifier:"
+cat >> my_new_role/molecule/default/molecule.yml "  name: ansible"
